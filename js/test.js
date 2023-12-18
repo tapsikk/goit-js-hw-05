5b9fd7d5dd05072cd64eace8fa4b5a2b5e5d2c01
tapsikk <118443898+tapsikk@users.noreply.github.com>
2023-12-18 13:23:48 +0200

    Initial commit

---
+++ js/test.js
@@ -0,0 +1,10 @@
+const players = [
+  { name: "Mango", playtime: 1270, gamesPlayed: 4 },
+  { name: "Poly", playtime: 469, gamesPlayed: 2 },
+  { name: "Ajax", playtime: 690, gamesPlayed: 3 },
+  { name: "Kiwi", playtime: 241, gamesPlayed: 1 },
+];
+
+const totalAveragePlaytimePerGame = players.reduce((total, time) => total + time.playtime/time.gamesPlayed, 0);
+
+console.log(totalAveragePlaytimePerGame);
